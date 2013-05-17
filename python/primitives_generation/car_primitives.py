19239146b4dd644b7ee84d9e5c2c28b8b2a17a48
lorenzoriano <lorenzo.riano@gmail.com>
2013-05-17 09:34:37 -0700

    using the front_axle as a reference for python classes

--- python/primitives_generation/car_primitives.py
+++ python/primitives_generation/car_primitives.py
@@ -4,7 +4,7 @@ import numpy as np
 from matplotlib import pylab
 
 def cont2disc(x, numofbins, max_v, min_v):
-    """Discretize an array x into numbins elements. The number will be 
+    """Discretize an array x into numbins elements. The number will be
     fixed between min_v and max_v.
     """
     x[x > max_v] = max_v
@@ -15,7 +15,7 @@ def bin_angle(x, numofbins):
     """Bins an array of angles x in numbins elements.  Returns the indexes of
     the bins (that is, discrete values).
     """
-    return cont2disc(x, numofbins, 2*np.pi, 0) 
+    return cont2disc(x, numofbins, 2*np.pi, 0)
 def continous_angle(x, numofbins):
     """Transforms a binned angle (as returned by bin_angle) into a continous
     angle.
@@ -24,7 +24,7 @@ def continous_angle(x, numofbins):
 def disc_angle(x, numofbins):
     """Discretizes an array of angles x into numbins elements. It applies
     bin_angle and then continous_angle.
-    """    
+    """
     binned = bin_angle(x, numofbins)
     disc = continous_angle(binned, numofbins)
     return disc
@@ -34,7 +34,7 @@ class CarPrimitives:
         f = open(cfgfile, "r")
         cfg = yaml.load(f)
         f.close()
-        
+
         self.car = carode.Car(cfg["car_length"],
                               cfg["max_v"],
                               cfg["min_v"],
@@ -58,29 +58,29 @@ class CarPrimitives:
                         only_endpoints = True):
         """Create a map of all the points reachable by sampling the velocities
         (linear and angular).
-        
+
         Parameters:
         multiple_thetas: if True multiple orientations will be considered
         only_endpoints: if True only the final position of a trajectory is considered
         """
-        
+
         car = self.car
         if multiple_thetas:
             all_thetas = np.linspace(0, 2*np.pi, self.theta_bins)
         else:
             all_thetas = [0]
-            
+
         #make sure we add the zero angles
         #theoreticallly it shouldn't be necessary if the angles are symmetric around
         #0 and the number of bins is odd.
-        
+
         all_thetas = np.union1d(all_thetas, [0])
-        all_ws = np.union1d(np.linspace(car.min_steer, car.max_steer, 
+        all_ws = np.union1d(np.linspace(car.min_steer, car.max_steer,
                                         self.theta_bins),
                             [0])
         #not necessary to add a zero here, the car has to move
         all_vs = np.linspace(car.min_vel, car.max_vel, self.v_bins)
-        
+
         all_points = []
         for theta in all_thetas:
             #rotating the car
@@ -89,45 +89,45 @@ class CarPrimitives:
                 for w in all_ws:
                     car.set_control(v, w)
                     if only_endpoints:
-                        traj = car.simulate(self.max_duration)                    
-                        all_points.append(traj[-1,:])    
+                        traj = car.simulate(self.max_duration)
+                        all_points.append(traj[-1,:])
                     else:
-                        traj = car.simulate(self.max_duration, 0.1)                    
+                        traj = car.simulate(self.max_duration, 0.1)
                         all_points.extend(traj)
-        
-        all_points =  np.array(all_points)    
+
+        all_points =  np.array(all_points)
         self.continous_points = all_points
         return all_points
-        
+
     def lattice_from_points(self, points):
         """Creates a lattice of points by discretizing the set of points passed in."""
         map_resolution = self.map_resolution
         theta_bins = self.theta_bins
-        
+
         disc_x = np.round(points[:, 0]/map_resolution) * map_resolution
         disc_y = np.round(points[:, 1]/map_resolution) * map_resolution
         disc_theta = disc_angle(points[:, 2], theta_bins)
         point_set = set((x, y, th) for x,y,th in zip(disc_x, disc_y, disc_theta)
-                        if abs(x) > self.map_resolution/2 or 
+                        if abs(x) > self.map_resolution/2 or
                         abs(y) > self.map_resolution/2)
-        
+
         #remove origin
         #filtered_point_set = filter( lambda x: abs(x[0])>self/map_resolution/2 and
                                                    #abs(x[1])>self/map_resolution/2,
                                                    #point_set)
-                                                   
-        points = np.array(list(point_set))   
+
+        points = np.array(list(point_set))
         self.lattice  = points
         return points
-        
+
     def create_primitives(self):
         """Creates a set of primitives. The map is first discretized to a lattice,
-        then for each cell a primitive is calculated. Depending on the 
+        then for each cell a primitive is calculated. Depending on the
         discretization, many cells might fail to have resulting primitive.
-        
+
         Returns the primitives and the failed cells
         """
-        
+
         print_steps = 20
         successes = 0
         tot_attempts = 0
@@ -140,10 +140,10 @@ class CarPrimitives:
         continous_points = self.create_grid_map(multiple_thetas=False,
                                                 only_endpoints=True)
         cells = self.lattice_from_points(continous_points)
-        
+
         for cell in cells:
             tot_attempts += 1
-            try:        
+            try:
                 control, _, imode = self.car.find_primitive_slsqp(cell, time_bounds)
             except ValueError:
                 print "======================"
@@ -151,14 +151,14 @@ class CarPrimitives:
                 print "Time bounds: ", time_bounds
                 print "======================"
                 failed_destinations.append(cell)
-                continue            
-            
+                continue
+
             if imode != 0: #optimization didn't work, skipping this
                 continue
-            
+
             #checking the error now
             self.car.set_control(control[0], control[1])
-            traj = self.car.simulate(control[2])[-1,:]        
+            traj = self.car.simulate(control[2])[-1,:]
             if ((abs(traj[0] - cell[0]) < self.map_resolution/2.) and
                 (abs(traj[1] - cell[1]) < self.map_resolution/2.) and
                 (abs(carode.diff_angle(traj[2], cell[2])) < theta_bin_size/2.)
@@ -168,49 +168,49 @@ class CarPrimitives:
                 succeded_destinations.append(cell)
             else:
                 failed_destinations.append(cell)
-            
+
             if tot_attempts % print_steps == 0:
-                print "Step %d/%d, Successes: %d, Failures: %d" % (tot_attempts, 
+                print "Step %d/%d, Successes: %d, Failures: %d" % (tot_attempts,
                                                                    len(cells),
-                                                                   successes, len(failed_destinations))        
-                
+                                                                   successes, len(failed_destinations))
+
         self.primitives = np.array(primitives)
         self.failed_cells = np.array(failed_destinations)
-        
+
         if self.show_plots:
             succeded_destinations = np.array(succeded_destinations)
-            #plot of continous points            
+            #plot of continous points
             pylab.figure()
             pylab.plot(continous_points[:,0], continous_points[:,1], '.')
             pylab.title("Reachable points (continous). # points: %d" % (len(continous_points)))
-            
+
             #discrete points, with red failures
             pylab.figure()
             pylab.plot(succeded_destinations[:,0], succeded_destinations[:,1], '.')
             pylab.plot(self.failed_cells[:,0], self.failed_cells[:,1], 'rx')
-            
+
             #now showing the trajectories
             trajectories = self.execute_primitives(self.primitives)
             for traj in trajectories:
-                pylab.plot(traj[:,0], traj[:,1]) 
-            
+                pylab.plot(traj[:,0], traj[:,1])
+
             pylab.title("Reachable points (lattice). # primitives: %d" % (len(self.primitives)))
-            pylab.show()                      
-        
-        return self.primitives, self.failed_cells        
-    
+            pylab.show()
+
+        return self.primitives, self.failed_cells
+
     def execute_primitives(self, primitives):
         """Executes each of the primitives and return a list of all the trajectories."""
-        
+
         all_trajectories = []
         for p in primitives:
             self.car.set_control(p[0], p[1])
             traj = self.car.simulate(p[2], 0.01)
             all_trajectories.append(traj)
-        
+
         return all_trajectories
-    
-    def cluster_primitives_from_angle(self, number_of_clusters, 
+
+    def cluster_primitives_from_angle(self, number_of_clusters,
                                       primitives=None,
                                       store_result = False):
         if primitives is None:
@@ -218,38 +218,38 @@ class CarPrimitives:
 
         #reducing some noise with roundoff
         primitives = np.round(primitives, 3)
-        
+
         res_primitives = []
         #finding the classes
         angles = primitives[:,1]
         bins = np.linspace(angles.min(), angles.max(), number_of_clusters)
         classes = np.digitize(angles, bins)
-        
+
         #selecting candidates among classes
         for c in range(number_of_clusters+1):
             inclass = primitives[classes == c]
             if len(inclass) == 0:
                 continue
-            
+
             #distance traveled is speed * time
             distances = inclass[:,0] * inclass[:,2]
-            
-            #distinguish between positive and negative for backward movements            
+
+            #distinguish between positive and negative for backward movements
             forward_primitives = inclass[distances >=0, :]
             backward_primitives = inclass[distances < 0, :]
-            
+
             #find the minimum for positive primitives
             if len(forward_primitives):
                 distances = forward_primitives[:,0] * forward_primitives[:,2]
                 i = np.argmin(distances)
                 res_primitives.append(forward_primitives[i,:])
-            
+
             #find the maximum for backward primitives
             if len(backward_primitives):
                 distances = backward_primitives[:,0] * backward_primitives[:,2]
                 i = np.argmax(distances)
                 res_primitives.append(backward_primitives[i,:])
-        
+
         new_primitives  = np.array(res_primitives)
         if store_result:
             self.primitives = new_primitives
@@ -260,46 +260,47 @@ class CarPrimitives:
             pylab.figure()
             if self.lattice is not None:
                 pylab.plot(self.lattice[:,0], self.lattice[:,1], '.')
-                
+
                 #now showing the trajectories
                 trajectories = self.execute_primitives(new_primitives)
                 for traj in trajectories:
-                    pylab.plot(traj[:,0], traj[:,1]) 
-                
+                    pylab.plot(traj[:,0], traj[:,1])
+
                 pylab.title("Reduced primitives. # primitives: %d" % (len(new_primitives)))
                 pylab.show()
-        
+
         return new_primitives
-    
-    def calculate_primitive_costs(self, primitives=None, number_of_classes = 3, 
+
+    def calculate_primitive_costs(self, primitives=None,
+                                  number_of_classes = 3,
                                   backward_multiplier = 2):
-        """Calculates the cost for each primitive. The main idea is that the 
+        """Calculates the cost for each primitive. The main idea is that the
         higher the steering, the more a primitive costs. Backward movements
         are assigned a higher cost too. In practice this is obtained by clustering
         the angles in number_of_classes bins, each of them with an increasing
         cost. The cost for backward movements is then multiplied by
         backward_multiplier.
         """
-        
+
         if primitives is None:
             primitives = self.primitives
 
         #the absolute curvature is what matters
         abs_angles = np.abs(primitives[:,1])
         #weird behavior of digitize
-        bins = np.linspace(abs_angles.min(), abs_angles.max()+0.001, 
+        bins = np.linspace(abs_angles.min(), abs_angles.max()+0.001,
                         number_of_classes+1)
         costs = np.digitize(abs_angles, bins)
         costs[ primitives[:,0] < 0] *= backward_multiplier
 
         #now calculating the distances
         distances = np.abs(primitives[:,0] * primitives[:,2]) / self.map_resolution
-        
+
         self.primitives_costs = costs * distances
-        
+
         self.primitives_costs = np.array(self.primitives_costs, dtype=np.int)
         return self.primitives_costs
-    
+
     def write_primitives(self, filename = None):
         motions = []
         for p, c in zip(self.primitives, self.primitives_costs):
@@ -321,7 +322,7 @@ class CarPrimitives:
     def write_world_file(self, filename = None):
         data = {}
         data["car_length"] = self.car.length
-        data["max_v"] = self.car.max_vel         
+        data["max_v"] = self.car.max_vel
         data["min_v"] = self.car.min_vel
         data["max_steering_angle"] = self.car.max_steer
         data["min_steering_angle"] = self.car.min_steer
@@ -330,50 +331,50 @@ class CarPrimitives:
         data['map_resolution'] = self.map_resolution
         data['v_bins'] = self.v_bins
         data['theta_bins'] = self.theta_bins
-        
+
         if filename is not None:
             f = open(filename, "w")
             return yaml.dump(data, f)
         else:
-            return yaml.dump(data)        
-        
-        return data        
-    
+            return yaml.dump(data)
+
+        return data
+
     def interpolate_waypoints(self, steps, intermediate_steps):
         """Given a series of waypoints, represented as a matrix with
         three columns (x, y, theta), interpolate between them by finding
         the best control from point to point and apllying it for intermediate_steps
         times.
-        
+
         Returns:
         the interpolated trajectory as a matrix
         """
         traj = []
         start = steps[0,:]
         traj.append(start)
-        time_bounds = [self.min_duration, self.max_duration]        
+        time_bounds = [self.min_duration, self.max_duration]
         for waypoint in steps[1:,:]:
-            self.car.x = start[0]
-            self.car.y = start[1]
+            self.car.front_x = start[0]
+            self.car.front_y = start[1]
             self.car.th = start[2]
-        
+
             (control, err, _) = self.car.find_primitive_slsqp_euler(waypoint, time_bounds)
             self.car.set_control(control[0], control[1])
             dt = control[2] / intermediate_steps
             interp_steps = self.car.simulate(control[2], dt)
             traj.append(interp_steps[1:,])
-            start = interp_steps[-1,:]            
-            
+            start = interp_steps[-1,:]
+
         return np.vstack(traj)
-    
-    def run(self, number_of_clusters, number_of_cost_classes, 
+
+    def run(self, number_of_clusters, number_of_cost_classes,
             destination_filename = None):
         self.create_primitives()
-        self.cluster_primitives_from_angle(number_of_clusters, 
+        self.cluster_primitives_from_angle(number_of_clusters,
                                            store_result=True)
-        self.calculate_primitive_costs(number_of_cost_classes)        
+        self.calculate_primitive_costs(number_of_classes = number_of_cost_classes)
         return self.write_primitives(destination_filename)
-            
+
 
 if __name__ == "__main__":
     import cPickle
--- python/primitives_generation/carode.py
+++ python/primitives_generation/carode.py
@@ -12,111 +12,140 @@ def norm_angle(x):
     return angles.normalize(x, 0, 2*pi)
 
 class Car(object):
+    """A simple car simulator with the capabilities of deriving motion primitives.
+
+    The simulation model is taken from LaValle's book, and follows the equations:
+    dx = v cos(th)
+    dy = v sin(th)
+    dth = v tan(w) / L
+
+    where x,y is the car's rear axle position, th is the steering angle, and
+    v,w are the controlled velocity and steering angle.
+
+    Note that the positions always refer to the front axle, and the conversion
+    to the rear one is handled by the simulator.
+    """
     def __init__(self, length, max_vel, min_vel, max_steer, min_steer,
                  x=0, y=0, th=0, lin_vel = 0, steer_angle=0):
         self.length = length
-        self.x = x
-        self.y = y
+        self.front_x = x
+        self.front_y = y
         self.th = th
         self.lin_vel = lin_vel
         self.steer_angle = steer_angle
-        
+
         self.max_vel = max_vel
         self.min_vel = min_vel
         self.max_steer = max_steer
         self.min_steer = min_steer
-        
+
         self.control_vel = 0
         self.control_angle = 0
-        
+
     def simulate(self, duration, time_step=1.0):
-        initial_state = [self.x, self.y, self.th]        
-        
+        """Simulate the car for a given duration.
+
+        The final returned trajectory will havethe initial and the ending
+        position, plus all the in-between poses calculated each specified
+        time_step (default 1.0).
+
+        Before using the simulate function use set_control to specify the
+        applied control. The car's position is not changed by this function.
+        """
+        #convert to rear axle
+        initial_state = [self.front_x - self.length * cos(self.th),
+                         self.front_y - self.length * sin(self.th),
+                         self.th]
+
         t = np.arange(0, duration, time_step)
         #make sure we add the final time
         if not np.allclose(t[-1], duration):
             t = np.hstack((t, duration))
-        
-        integrator = ode(self.integrate)
-        integrator.set_integrator('vode', with_jacobian=False, nsteps=1000)        
-        y = initial_state 
-        traj = [initial_state]
+
+        integrator = ode(self.__integrate)
+        integrator.set_integrator('vode', with_jacobian=False, nsteps=1000)
+        y = initial_state
+        traj = [[self.front_x, self.front_y, self.th]]
         for step, prev_t in zip(t[1:], t[:-1]):
             integrator.set_initial_value(y, prev_t)
             y = integrator.integrate(step)
             if not integrator.successful():
-                raise ValueError("Error during integration!")            
+                raise ValueError("Error during integration!")
             y[2] = norm_angle(y[2])
-            traj.append(y)
-        
+            #convert back to front axle
+            front_axle = [y[0] + self.length * cos(self.th),
+                          y[1] + self.length * sin(self.th),
+                          y[2]
+                          ]
+            traj.append(front_axle)
+
         return np.array(traj)
-        
-        #ret = odeint(self.integrate, initial_state, t)
-        #return ret
-    
+
     def set_control(self, vel, steer):
         norm_steer = angles.normalize(steer, -2*pi, 2*pi)
-        
+
         self.control_vel = self.lin_vel + vel
         if self.control_vel > self.max_vel:
             self.control_vel = self.max_vel
         if self.control_vel > self.max_vel:
-            self.control_vel = self.min_vel        
-        
+            self.control_vel = self.min_vel
+
         self.control_angle = self.steer_angle + norm_steer
         if self.control_angle > self.max_steer:
             self.control_angle = self.max_steer
         if self.control_angle > self.max_steer:
-            self.control_angle = self.min_steer 
-        
-    def integrate(self,  t, state):
+            self.control_angle = self.min_steer
+
+    def __integrate(self,  t, state):
         theta = state[2]
-        
+
         dxdt = np.empty(3)
         dxdt[0] = self.control_vel * cos(theta ) #x
         dxdt[1] = self.control_vel * sin(theta) #y
         dxdt[2] = self.control_vel * tan(self.control_angle) /  self.length #th
-        
+
         dxdt[2] = dxdt[2]
         return dxdt
-    
-    def euler_step(self, state, t):
-        theta = norm_angle(state[2])
-        
-        dxdt = np.empty(3)
-        dxdt[0] = state[0] + self.control_vel * cos(theta )*t #x
-        dxdt[1] = state[1] + self.control_vel * sin(theta)*t #y
-        dxdt[2] = state[2] + self.control_vel * tan(self.control_angle) /  self.length * t #th
-        
-        dxdt[2] = norm_angle(dxdt[2])
-        
-        return dxdt
-    
+
     def find_primitive_slsqp(self, end_pos, time_bounds,
                              num_attempts = 10):
-        
+        """Finds a primitive that leads the car to a specified position.
+
+        The user specifies end_pos = (x,y,th) and the
+        time_bounds = [min_time, max_time], i.e. how long should the control be
+        executed for. A primitive is a triple of (v,w,t), i.e. velocity, steering
+        angle and duration to bring the car from it's current position to the
+        desired end_pos. Internally it uses scipy fmin_slsqp to find a solution.
+
+        Several starting conditions can be specified with the num_attempts
+        parameter.
+
+        Returns:
+        a primitive (v,w,t) the brings the car from its current position to be
+        as close as possible to end_pos.
+        """
+
         #bounds
         vel_bounds = (self.min_vel + self.lin_vel, self.max_vel - self.lin_vel)
         steer_bounds = (self.min_steer + self.steer_angle, self.max_steer + self.steer_angle)
-        #time_bounds = [0.1, 1.0]
-        
+
         bounds = [vel_bounds, steer_bounds, time_bounds]
         end_pos = np.array(end_pos)
-        
+
         def position_error(control):
             required_time = control[2]
             if required_time <= 0:
                 print "Warning, setting a time <=0"
                 return np.sum(end_pos**2)
-            
+
             self.set_control(control[0], control[1])
-            
+
             traj = self.simulate(required_time)
             final_pos = traj[-1,:]
             err_x = (end_pos[0] - final_pos[0])
             err_y = (end_pos[1] - final_pos[1])
             err_th = diff_angle(end_pos[2], final_pos[2]) #special angles treatment
-            
+
             err = err_x**2 + err_y**2 + err_th**2
             return err
 
@@ -138,52 +167,68 @@ class Car(object):
                                       )
             if ret[1] < best_ret[1]:
                 best_ret = ret
-            
-        control, err, _, imode, _ = best_ret        
+
+        control, err, _, imode, _ = best_ret
         return (control, err, imode)
-    
+
     def find_primitive_slsqp_euler(self, end_pos, time_bounds,
                                  num_attempts = 10):
-            
+        """Similar to find_primitive_slsqp, but the gradient is provided rather
+        than approximated. It is faster than find_primitive_slsqp, but it might
+        not have the correct solution as the gradient is approximated using a
+        first order Euler approximation.
+
+        See find_primitive_slsqp for the parameters and return values.
+        """
+
         #bounds
         vel_bounds = (self.min_vel + self.lin_vel, self.max_vel - self.lin_vel)
         steer_bounds = (self.min_steer + self.steer_angle, self.max_steer + self.steer_angle)
         #time_bounds = [0.1, 1.0]
-        
+
         bounds = [vel_bounds, steer_bounds, time_bounds]
-        end_pos = np.array(end_pos)
-        
+
+        #convert end_pos to rear_axle representation
+        end_pos = np.array([end_pos[0] - self.length * cos(self.th),
+                            end_pos[1] - self.length * sin(self.th),
+                            end_pos[2]
+                            ])
+
+        rear_x = self.front_x - self.length * cos(self.th)
+        rear_y = self.front_y - self.length * sin(self.th)
+
         def position_error(control):
             v, w, t = control
             if t <= 0:
                 print "Warning, setting a time <=0"
                 return np.sum(end_pos**2)
-            
+
             thp = self.th + v/self.length*t*tan(w)
-            xp = self.x + v*t*cos(thp)
-            yp = self.y + v*t*sin(thp)
-            
+            xp = rear_x + v*t*cos(thp)
+            yp = rear_y + v*t*sin(thp)
+
             err_x = (end_pos[0] -xp)
             err_y = (end_pos[1] - yp)
             err_th = diff_angle(end_pos[2], thp) #special angles treatment
-            
+
             err = err_x**2 + err_y**2 + err_th**2
             return err
-        
+
         goal_x = end_pos[0]
         goal_y = end_pos[1]
         goal_th = end_pos[2]
-        x0 = self.x
-        y0 = self.y
+        x0 = rear_x
+        y0 = rear_y
         th0 = self.th
-        l = self.length        
+        l = self.length
         Abs = fabs
         def fprime(control):
+            #magic coming from simpy
             v, w, t = control
             dv = -2*t*(goal_x - t*v*cos(th0) - x0)*cos(th0) - 2*t*(goal_y - t*v*sin(th0) - y0)*sin(th0) - 2*t*(-Abs(-Abs(goal_th - th0 - t*v*tan(w)/l) + pi) + pi)*tan(w)*sign(-Abs(goal_th - th0 - t*v*tan(w)/l) + pi)*sign(goal_th - th0 - t*v*tan(w)/l)/l
             dw = -2*t*v*(tan(w)**2 + 1)*(-Abs(-Abs(goal_th - th0 - t*v*tan(w)/l) + pi) + pi)*sign(-Abs(goal_th - th0 - t*v*tan(w)/l) + pi)*sign(goal_th - th0 - t*v*tan(w)/l)/l
             dt = -2*v*(goal_x - t*v*cos(th0) - x0)*cos(th0) - 2*v*(goal_y - t*v*sin(th0) - y0)*sin(th0) - 2*v*(-Abs(-Abs(goal_th - th0 - t*v*tan(w)/l) + pi) + pi)*tan(w)*sign(-Abs(goal_th - th0 - t*v*tan(w)/l) + pi)*sign(goal_th - th0 - t*v*tan(w)/l)/l
-            
+
             return [dv, dw, dt]
 
         #initial_control = [vel_bounds[1], 0, time_bounds[0]]
@@ -203,9 +248,9 @@ class Car(object):
                                       )
             if ret[1] < best_ret[1]:
                 best_ret = ret
-            
-        control, err, _, imode, _ = best_ret        
-        return (control, err, imode)    
+
+        control, err, _, imode, _ = best_ret
+        return (control, err, imode)
 
 if __name__ == "__main__":
     max_vel = 1.0
@@ -215,8 +260,8 @@ if __name__ == "__main__":
     car = Car(0.95, max_vel, min_vel, max_steer, min_steer,
                      lin_vel=0.0, steer_angle=0)
     car.set_control(1070.86996923,   247.26455191)
-    
+
     max_time =  2560.00974537
     #steps = np.linspace(1, 2560.00974537, 100)
-    traj2 = car.simulate( max_time)    
+    traj2 = car.simulate( max_time)
     print traj2
